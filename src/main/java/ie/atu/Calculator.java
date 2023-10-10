fb2ed9f490bfb8acebf5c016ccad22265e07ae9f
Johanan26 <g00419446@atu.ie>
2023-10-10 14:04:36 +0100

    Added a new multiplying numbers

--- src/main/java/ie/atu/Calculator.java
+++ src/main/java/ie/atu/Calculator.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Calculator {
     public static void main(String[] args) {
     add();
+    multiply();
 
     }
     public static void add() {
@@ -16,6 +17,17 @@ public class Calculator {
         int secondNumber = inputs.nextInt();
 
         int total = firstNumber + secondNumber;
+        System.out.println("The total " + total);
+    }
+    public static void multiply() {
+        System.out.println("Please Enter Your First Number:");
+        Scanner inputs = new Scanner(System.in);
+        int firstNumber = inputs.nextInt();
+
+        System.out.println("Please Your Second Number:");
+        int secondNumber = inputs.nextInt();
+
+        int total = firstNumber * secondNumber;
         System.out.println("The total " + total);
     }
 }
